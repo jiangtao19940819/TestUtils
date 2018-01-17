d5be986486352fc53e6bc13309a4555d672e2f99
jiangtao <jiangtao@baiwang.com>
2018-01-17 19:46:11 +0800

    111

--- src/main/java/com/baiwang/MyTestUtils/App.java
+++ src/main/java/com/baiwang/MyTestUtils/App.java
@@ -11,6 +11,7 @@ public class App
 	
     public static void main( String[] args ) throws Exception{
     	   try{
+    		   System.out.println(System.getProperty("user.dir"));
     		  Client.run(); 
     	   }catch(Exception e){
     		   e.printStackTrace();
